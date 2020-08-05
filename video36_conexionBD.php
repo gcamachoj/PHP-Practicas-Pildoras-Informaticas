d459037011c30d0e76b6f565076daa3957e4138e
Guillero Camacho <guiller123camacho.jara@gmail.com>
2020-08-05 11:12:12 -0500

    hoy se vio la conexion a mysql y se hizo video 36.php

---
+++ video36_conexionBD.php
@@ -0,0 +1,26 @@
+<?php
+// SERVIDOR SLEFICACES
+
+$db_host="host17.latinoamericahosting.com";
+$db_name="sleficac_taller";
+$db_user="sleficac_gcamach";
+$db_contra="Logistica01*";
+
+
+
+// SERVIDOR LOCAL 
+/*
+$db_host="localhost";
+$db_name="sleficac_taller";
+$db_user="root";
+$db_contra="";
+*/
+// tener en cuenta en la conexion que el orden de las variables de conexion es el siguiente.
+$conexion=mysqli_connect($db_host,$db_user,$db_contra,$db_name);
+
+ echo $conexion ? "conexion exitosa" : "error durante la conexion";
+
+$query = "select * from Actividades";
+
+
+?>
