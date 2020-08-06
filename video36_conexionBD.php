da9bc60ee1a751c0fb81d1f1952f7b2d90f9cf46
Guillero Camacho <guiller123camacho.jara@gmail.com>
2020-08-06 18:24:34 -0500

    video 36

--- video36_conexionBD.php
+++ video36_conexionBD.php
@@ -1,4 +1,12 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+
 <?php
+header("Content-Type: text/html;charset=utf-8");
 // SERVIDOR SLEFICACES
 
 $db_host="host17.latinoamericahosting.com";
@@ -16,11 +24,32 @@ $db_user="root";
 $db_contra="";
 */
 // tener en cuenta en la conexion que el orden de las variables de conexion es el siguiente.
+
 $conexion=mysqli_connect($db_host,$db_user,$db_contra,$db_name);
 
- echo $conexion ? "conexion exitosa" : "error durante la conexion";
+mysqli_set_charset($conexion, "utf8");
+
+
+
+if(mysqli_connect_errno()){
+    echo "Fallo al conectar con la base de datos";
+    exit();
+}
+ echo $conexion ? "<br>conexion exitosa<br>" : "<br>error durante la conexion<br>";
+
+ mysqli_set_charset($conexion, "utf-8");
 
 $query = "select * from Actividades";
+$resultados = mysqli_query($conexion, $query);
+while($fila=mysqli_fetch_row($resultados))
+{
 
-
+echo $fila[0] . " ";
+echo $fila[1] . " ";
+echo $fila[2] . " ";
+echo $fila[3]." ";
+echo "<br>";
+}
+mysqli_close($conexion);
 ?>
+</body>
